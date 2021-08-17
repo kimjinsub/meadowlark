17c7b9ecd00757054753db59719dd2036a1af419
kimjinsub <kimjinsub01@naver.com>
2021-08-18 00:42:25 +0900

    jest

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,7 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
-const fortune = require('./lib/fortune')/* ./ : node_modules 디렉터리에서 모듈을 찾지 말라는 의미. 이 부분 생략시 임포트 실패. */
+const handlers = require('./lib/handlers')
 
 
 // 핸들바 뷰 엔진 설정
@@ -12,25 +12,20 @@ app.engine('handlebars', expressHandlebars({
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname+'/public'))
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune:fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port}; ` +
-    `press Ctrl-C to terminate.`))
+if(require.main === module){
+    app.listen(port, () => console.log(
+        `Express started on http://localhost:${port}; ` +
+        `press Ctrl-C to terminate.`))
+}else{
+    module.exports = app
+}
